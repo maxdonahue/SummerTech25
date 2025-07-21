f11dbcf311b0878f70fd08db33e283300c5edc8f
maxdonahue <donahuemax@icloud.com>
2025-07-21 17:30:53 +0000

    calculator added

--- calculator.py
+++ calculator.py
@@ -1,22 +1,29 @@
-num1 = int(input("enter the first number "))
-num2 = int(input("enter the second number "))
-operation = input("enter the operation ")
+while(True):
+    num1 = int(input("enter the first number "))
+    num2 = int(input("enter the second number "))
+    operation = input("enter the operation ")
 
 
-if operation == '+':
-    print(num1 + num2)
+    if operation == '+':
+        print(num1 + num2)
 
-elif operation == "*":
-    print(num1*num2)
-        
-elif operation == "/":
-    print(num1 / num2)
+    elif operation == "*":
+        print(num1*num2)
+            
+    elif operation == "/":
+        print(num1 / num2)
 
-elif operation == "-":
-    print(num1 - num2)
-else:
-    print("not an option")
+    elif operation == "-":
+        print(num1 - num2)
+    else:
+        print("not an option")
 
-
-
-    
+    print("would you like to continue?")
+    answer = input("yes or no?")
+    if answer == "yes":
+        continue
+    elif answer == "no":
+        break
+    else:
+        print("not an option. stopping calculator")
+        break
