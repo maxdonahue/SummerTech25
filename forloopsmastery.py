ae8037bd60fb08f742943b5637c49f110ae80fb1
maxdonahue <donahuemax@icloud.com>
2025-07-22 15:07:55 +0000

    rps

---
+++ forloopsmastery.py
@@ -0,0 +1,9 @@
+# for j in range (10):
+
+#     for x in range(10):
+#         print("* ", end="") 
+#     print("")
+
+
+for u in range (0, 11):
+    print(u)
---
+++ tictactoe.py
@@ -0,0 +1,53 @@
+from random import randint
+
+
+
+while (True):
+
+
+
+    print("rock paper scissors shoot")
+
+    computor = randint(0,2)
+
+    user = input("press [r] for rock, [p] for paper, [s] for scissor. ")
+
+
+
+    # win check
+    if computor == 0:
+        if user == "p":
+            print("you win computor chose rock")
+        elif user == "s":
+            print("you lose computor played rock") 
+        elif user == "r":
+            print("it's a tie computor chose rock")
+        else:
+            print("not an option")
+
+    elif computor == 1:
+        if user == "r":
+            print ("you lose computor chose paper")
+        elif user == "p":
+            print("you tied computor chose paper")
+        elif user == "s":
+            print("you won computor chose scissor")
+
+    elif computor == 2:
+        if user == "r":
+            print("you won computor chose scissors")
+        elif user == "p":
+            print("you lost")
+        elif user == "s":
+            print("you tied computor chose scissor")
+
+    answer = input("Do you want to play more? y/n ")
+    
+
+    if answer == "y":
+        continue
+    elif answer == "n":
+        break
+    else:
+        print("Invalid, shutting down game")
+        break
